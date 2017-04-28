b90ea568e4020da4c3297c59e8d56cfe2483a993
Rafael Diaz Handal <rafadiazh98@gmail.com>
2017-04-28 13:25:48 -0600

    Cree clase Rusos

---
+++ Lab2_RafaelDiaz/src/lab2_rafaeldiaz/Lab2_RafaelDiaz.java
@@ -0,0 +1,17 @@
+
+package lab2_rafaeldiaz;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Lab2_RafaelDiaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
